91684ea3c5d9c37e6c9495077f6e205cb9cdcf91
quentin <quentinmaz@hotmail.fr>
2024-11-26 11:35:05 +0100

    changed autodoc to autodoc2.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,12 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['autodoc2']
+
+autodoc2_packages = [
+    "../src"
+]
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
--- docs/references.rst
+++
@@ -1,6 +0,0 @@
-References
-============
-
-The ``src`` folder contains the module ``multiply`` module which provides the following function:
-
-.. autofunction:: multiply.multiply
